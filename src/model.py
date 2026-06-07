9368a2bb0bf3badc4b290a79d856b998aa53a03a
orzel320 <52106515+orzel320@users.noreply.github.com>
2026-06-07 20:17:15 +0200

    Corrected error in noise implementation

--- src/model.py
+++ src/model.py
@@ -149,7 +149,8 @@ def initialize_weights(model):
         if isinstance(m, (nn.Conv2d, nn.ConvTranspose2d)):
             nn.init.normal_(m.weight.data, 0.0, 0.02)
         elif isinstance(m, (nn.BatchNorm2d, nn.InstanceNorm2d)):
-            nn.init.normal_(m.weight.data, 1.0, 0.02)
+            if m.weight is not None:
+                nn.init.normal_(m.weight.data, 1.0, 0.02)
             if m.bias is not None:
                 nn.init.constant_(m.bias.data, 0)
         elif isinstance(m, nn.Linear):
